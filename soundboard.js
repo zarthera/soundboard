4eb2a15c71e09953b9e77022776910bf66680583
zarthera <ztartera5@gmail.com>
2023-02-09 16:54:36 -0500

    Audio on click and shows which audio is playing

---
+++ soundboard.js
@@ -0,0 +1,18 @@
+function playAudio(clickedObject){
+    const dataNumber = clickedObject.getAttribute('data-number')
+    
+    const audio = document.querySelector(`audio[data-number="${dataNumber}"]`)
+    const sound = document.querySelector(`button[data-number="${dataNumber}"]`)
+
+    if(!audio)  { return }
+
+    const durationInMilliseconds = audio.duration * 1000
+
+    sound.classList.add('playing')
+    console.log(audio.duration)
+    audio.currentTime = 0;
+    audio.play();
+    
+    setTimeout(() => {sound.classList.remove('playing')}, durationInMilliseconds)
+       
+}
